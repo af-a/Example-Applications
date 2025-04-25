e66d820739a1210c2808692e8f3de047fb9204a0
Ahmed Abdelrahman <37597383+af-a@users.noreply.github.com>
2025-04-25 14:39:09 +0200

    [LandingScreenController] Modify to add new custom args

--- Python/UIControls/LandingScreenController.py
+++ Python/UIControls/LandingScreenController.py
@@ -3,14 +3,16 @@ from StartMenu.StartWindow import StartWindow
 
 
 class LandingScreenController():
-    def __init__(self):
+    def __init__(self, with_classifications_indicator=False, debug=False):
         self.startWindow = StartWindow(self)
-        self.collectWindow = CollectDataWindow(self)
+        self.collectWindow = CollectDataWindow(self, with_classifications_indicator=with_classifications_indicator)
 
         self.startWindow.show()
 
         self.curHeight = 900
         self.curWidth = 1400
+        
+        self.debug = debug
 
     def showStartMenu(self):
         self.collectWindow.close()
@@ -21,6 +23,6 @@ class LandingScreenController():
         if self.startWindow.plot_enabled.isChecked():
             self.collectWindow.plot_enabled = True
             self.collectWindow.AddPlotPanel()
-        self.collectWindow.SetCallbackConnector()
+        self.collectWindow.SetCallbackConnector(debug=self.debug)
         self.collectWindow.connect_callback()
         self.collectWindow.show()
